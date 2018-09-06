80ac5fc7dc1500f0b99ba6cfcae14f6a7acae417
Lily Wittle <lily.wittle@gmail.com>
2018-09-06 16:43:29 -0400

    Rating activity XML

---
+++ RatingProgress/app/src/main/java/io/github/lily_wittle/ratingprogress/RatingActivity.java
@@ -0,0 +1,13 @@
+package io.github.lily_wittle.ratingprogress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
