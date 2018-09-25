724cf039ebd9e708899ae6cff616e28ca3c7b044
Lily Wittle <lily.wittle@gmail.com>
2018-09-25 10:25:33 -0400

    List of Pictures done

--- ListofPictures/app/src/main/java/io/github/lily_wittle/listofpictures/MainActivity.java
+++ ListofPictures/app/src/main/java/io/github/lily_wittle/listofpictures/MainActivity.java
@@ -12,12 +12,9 @@ import android.widget.ImageView;
 import android.util.Log;
 import android.graphics.Color;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements OnItemClickListener {
-
-    private int currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +26,14 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
         theList.setOnItemClickListener(this);
 
         registerForContextMenu(findViewById(R.id.the_image));
+        Log.i("IN onCreate", "Created OK");
     }
     //-----------------------------------------------------------------------------
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater;
-
-        inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
+        Log.i("IN onCreateOptionsMenu", "Created options menu");
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
 
         return(true);
     }
@@ -45,9 +41,61 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     //-----------------------------------------------------------------------------
     public void onCreateContextMenu(ContextMenu menu,View view,
                                     ContextMenu.ContextMenuInfo menuInfo) {
-
+        Log.i("IN onCreateContextMenu", "Created context menu");
         super.onCreateContextMenu(menu,view,menuInfo);
         getMenuInflater().inflate(R.menu.context_menu,menu);
+    }
+
+    //-----------------------------------------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        ImageView theImage;
+        ListView theList;
+        View imageName;
+
+        theImage = findViewById(R.id.the_image);
+        theList = findViewById(R.id.the_list);
+
+        switch (item.getItemId()) {
+            case R.id.hide:
+                Log.i("IN onOptionsItemSelect", "Hid image");
+                theImage.setVisibility(View.INVISIBLE);
+                return(true);
+            case R.id.show:
+                Log.i("IN onOptionsItemSelect", "Showed image");
+                theImage.setVisibility(View.VISIBLE);
+                return(true);
+            case R.id.happy:
+                imageName = theList.getChildAt(0);
+                break;
+            case R.id.sad:
+                imageName = theList.getChildAt(1);
+                break;
+            case R.id.crazy:
+                imageName = theList.getChildAt(2);
+                break;
+            case R.id.angry:
+                imageName = theList.getChildAt(3);
+                break;
+            case R.id.crying:
+                imageName = theList.getChildAt(4);
+                break;
+            default:
+                return(super.onOptionsItemSelected(item));
+        }
+
+        if (item.isChecked()) {
+            Log.i("IN onOptionsItemSelect", "Disabled option");
+            imageName.setEnabled(false);
+            item.setChecked(false);
+        }
+        else {
+            Log.i("IN onOptionsItemSelect", "Enabled option");
+            imageName.setEnabled(true);
+            item.setChecked(true);
+        }
+        return(true);
     }
 
     //-----------------------------------------------------------------------------
@@ -57,15 +105,18 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
 
         theLL = findViewById(R.id.ll_background);
 
-        currentColor = item.getItemId();
+        int currentColor = item.getItemId();
         switch (currentColor) {
             case R.id.black:
+                Log.i("IN onContextItemSelect", "Set color to black");
                 theLL.setBackgroundColor(Color.BLACK);
                 return(true);
             case R.id.red:
+                Log.i("IN onContextItemSelect", "Set color to red");
                 theLL.setBackgroundColor(Color.RED);
                 return(true);
             case R.id.green:
+                Log.i("IN onContextItemSelect", "Set color to green");
                 theLL.setBackgroundColor(Color.GREEN);
                 return(true);
             default:
@@ -78,11 +129,16 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
 
         ListView theList;
         ImageView theImage;
+        View imageNameView;
+        String name;
 
         theList = findViewById(R.id.the_list);
         theImage = findViewById(R.id.the_image);
-                String pictureName = (String) theList.getItemAtPosition(position);
-                switch (pictureName) {
+        imageNameView = theList.getChildAt(position);
+        name = (String) theList.getItemAtPosition(position);
+
+            if (imageNameView.isEnabled()) {
+                switch (name) {
                     case "Happy":
                         Log.i("IN onItemClick", "Clicked happy");
                         theImage.setImageResource(R.drawable.happy);
@@ -103,8 +159,8 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                         Log.i("IN onItemClick", "Clicked crying");
                         theImage.setImageResource(R.drawable.crying);
                         break;
-
-        }
+                }
+            }
 
     }
 }
