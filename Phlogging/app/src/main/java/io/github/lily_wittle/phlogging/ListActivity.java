b61267beb555f5423d9c6a4289b8308b222ddbd7
Lily Wittle <lily.wittle@gmail.com>
2018-12-02 14:14:12 -0500

    Layout files

---
+++ Phlogging/app/src/main/java/io/github/lily_wittle/phlogging/ListActivity.java
@@ -0,0 +1,14 @@
+package io.github.lily_wittle.phlogging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // set content view
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
