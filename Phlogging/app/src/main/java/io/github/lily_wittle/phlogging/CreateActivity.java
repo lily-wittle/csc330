1944dc84cc7e12dfa586070ce093f67b1cda032f
Lily Wittle <lily.wittle@gmail.com>
2018-12-06 23:05:56 -0500

    Hints

--- Phlogging/app/src/main/java/io/github/lily_wittle/phlogging/CreateActivity.java
+++ Phlogging/app/src/main/java/io/github/lily_wittle/phlogging/CreateActivity.java
@@ -16,6 +16,7 @@ import android.os.Looper;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
